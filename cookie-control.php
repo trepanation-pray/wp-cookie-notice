e16c9d1bee6a1246b185a0185a9cb88806eddbca
Steve Hill <steve@pillorybarn.co.uk>
2021-11-12 11:06:06 +0000

    Version number

--- cookie-control.php
+++ cookie-control.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Cookie Control
   Description: EU compliant cookie control
-  Version: 2.1.2
+  Version: 2.1.3
   Author: Steven Hill
   Author URI: http://www.stevenhill.me
   License: GPL2
